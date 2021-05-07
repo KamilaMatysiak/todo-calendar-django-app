61fa2503c09d80ed157c4e8f82685de7cd7bef0c
xkamikoo <58092037+xkamikoo@users.noreply.github.com>
2021-05-08 00:15:23 +0200

    Add homepage

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="list"),
+    path('home', views.homepage, name="index"),
     path('update_task/<str:pk>/', views.updateTask, name="update_task"),
     path('delete/<str:pk>/', views.deleteTask, name="delete")
 ]
--- todo/settings.py
+++ todo/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import mimetypes
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'/static/')
+
+mimetypes.add_type('image/svg+xml', '.svg', True)
