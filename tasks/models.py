40ffb52d604366c7b8cd09beefe3835d4cbd12c7
OzzerTheHuman <kinga.jagodzinska@gmail.com>
2021-06-08 00:41:28 +0200

    Add Priority

--- tasks/models.py
+++ tasks/models.py
@@ -5,6 +5,19 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
+    High_priority = "H"
+    Medium_priority = "M"
+    Low_priority = "L"
+    Priorities = [
+        (Low_priority, "Low"),
+        (Medium_priority, "Medium"),
+        (High_priority, "High")
+    ]
+    priority = models.CharField(
+        max_length=10,
+        choices=Priorities,
+        default="Low"
+    )
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
 
