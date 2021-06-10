62988bd9114fe542099aec78fd05ec68fccde03c
Adrian Charkiewicz <adrcha2@st.amu.edu.pl>
2021-06-11 01:27:31 +0200

    now every information is displayed in task, including location and with_who

--- tasks/models.py
+++ tasks/models.py
@@ -11,4 +11,4 @@ class Task(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return f"{self.title}   {self.localization} {self.with_who}"
