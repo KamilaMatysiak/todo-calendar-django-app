5773f878dd6b3dd7458afb20c2dd11c981584d0e
Adrian Charkiewicz <adrcha2@st.amu.edu.pl>
2021-06-10 11:36:53 +0200

    added registration module

--- todo/settings.py
+++ todo/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'tasks',
+    'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 mimetypes.add_type('image/svg+xml', '.svg', True)
