34d77e99706a5e36157269eee656076d5fac7680
Adrian Charkiewicz <adrcha2@st.amu.edu.pl>
2021-06-11 13:43:09 +0200

    added datetime in older version of list

--- tasks/forms.py
+++ tasks/forms.py
@@ -7,6 +7,9 @@ class TaskForm(forms.ModelForm):
     title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Add new task...'}))
     localization = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Set localization'}), required=False)
     with_who = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'With who?'}), required=False)
+    date = forms.DateTimeField(widget=forms.DateTimeInput(format='%d-%m-%Y %H:%M',
+                                                          attrs={'class': 'myDateClass',
+                                                                 'placeholder': 'Select a date'}), required=False)
 
     class Meta:
         model = Task
