c19f1ab4ca7d8e2d8cbcc9468ec650cc6c433bf0
Adrian Charkiewicz <adrcha2@st.amu.edu.pl>
2021-06-11 13:48:56 +0200

    changed from datetime.now to  django auto_now=True

--- tasks/models.py
+++ tasks/models.py
@@ -2,12 +2,14 @@ from django.db import models
 import datetime
 
 # Create your models here.
+from isort.profiles import django
+
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
     localization = models.CharField(max_length=200)
     with_who = models.CharField(max_length=200)
-    date = models.DateField("Date", default=datetime.datetime.now())
+    date = models.DateField("Date", auto_now=True)
 
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
