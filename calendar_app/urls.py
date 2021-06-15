81fb371bdbf5f40805331e5782a020ba08c905c5
xkamikoo <58092037+xkamikoo@users.noreply.github.com>
2021-06-15 20:50:23 +0200

    Navigation between modules

--- calendar_app/urls.py
+++ calendar_app/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    # path('', views.home, name="home")
+    
     path('<int:year>/<int:month>/<int:day>', views.home, name="home"),
+    path('2020/6/12', views.home, name="home_empty"),
     path('', views.current_date),
     path('add_meeting', views.new_meeting, name="add_meeting"),
     path('edit/<int:pk>', views.edit_meeting, name='edit_meeting')
