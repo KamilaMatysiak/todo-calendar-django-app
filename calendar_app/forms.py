cc9d59e0d19754b8c660d98685949d5170886b88
Adrian Charkiewicz <adrcha2@st.amu.edu.pl>
2021-06-17 00:42:40 +0200

    placeholder for title is also in polish

--- calendar_app/forms.py
+++ calendar_app/forms.py
@@ -4,7 +4,7 @@ from .models import *
 
 
 class MeetingForm(forms.ModelForm):
-    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'New task'}))
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Nowe zadanie'}))
     time_start = forms.TimeField(widget=forms.TimeInput(format='%H:%M', attrs={'placeholder': '12:33 '}))
     time_end = forms.TimeField(widget=forms.TimeInput(format='%H:%M', attrs={'placeholder': '17:18'}))
 
