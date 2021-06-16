758974345b63076c0354ff24320bee5a63b239cb
Adrian Charkiewicz <adrcha2@st.amu.edu.pl>
2021-06-16 22:50:16 +0200

    a little more of translated time measurments

--- tasks/templatetags/poll_extras.py
+++ tasks/templatetags/poll_extras.py
@@ -5,8 +5,13 @@ register = template.Library()
 
 @register.filter
 def split_timeuntil(duration):
-    duration = duration.replace('hours', 'godziny') \
+    duration = duration.replace('hours', 'godzin') \
         .replace('minutes', 'minuty') \
         .replace('weeks', 'tygodnie') \
-        .replace('days', 'dni')
+        .replace('days', 'dni') \
+        .replace('day', 'dzien') \
+        .replace('hour', 'godzina') \
+        .replace('months', 'miesiecy') \
+        .replace('month', 'miesiac') \
+        .replace('minute', 'minuta')
     return duration
