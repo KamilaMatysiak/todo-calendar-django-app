6674f2498ce6d070f15e8209fc506bf15971eaaf
Adrian Charkiewicz <adrcha2@st.amu.edu.pl>
2021-06-17 00:38:16 +0200

    changed placeholders to examples of inputs

--- calendar_app/forms.py
+++ calendar_app/forms.py
@@ -4,9 +4,9 @@ from .models import *
 
 
 class MeetingForm(forms.ModelForm):
-    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Add new task...'}))
-    time_start = forms.TimeField(widget=forms.TimeInput(format='%H:%M', attrs={'placeholder': 'Add new task...'}))
-    time_end = forms.TimeField(widget=forms.TimeInput(format='%H:%M', attrs={'placeholder': 'Add new task...'}))
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'New task'}))
+    time_start = forms.TimeField(widget=forms.TimeInput(format='%H:%M', attrs={'placeholder': '12:33 '}))
+    time_end = forms.TimeField(widget=forms.TimeInput(format='%H:%M', attrs={'placeholder': '17:18'}))
 
     class Meta:
         model = Meeting
