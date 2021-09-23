436bcec5916af3d3169841ebeed9f83acfc9568b
OzzerTheHuman <kinga.jagodzinska@gmail.com>
2021-09-23 12:34:48 +0200

    Show correct notification after pushing button

--- geolocation/views.py
+++ geolocation/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 # from .models import Measurement
 # from .forms import MeasurementModelForm
 from geopy.geocoders import Nominatim
@@ -16,8 +16,11 @@ from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from webpush import send_user_notification
 import json
+from django.http import HttpResponse
+from django.urls import reverse
+from django.template import loader
 
-def location(request):
+def location(request, lat, lon):
     """Shows a map with starting point of user, based on users location. Also shows the locations of tasks and their priorities.
 
     Args:
@@ -26,73 +29,39 @@ def location(request):
         .html file with a map of user starting point and places with task assigned to them
 
     """
-    webpush_settings = getattr(settings, 'WEBPUSH_SETTINGS', {})
-    vapid_key = webpush_settings.get('VAPID_PUBLIC_KEY')
-    user = request.user
-
-
-    # distance = None
-    # destination = None
-
-    # nie odkomentowuj bo on psuje wszystko
-    # obj = get_object_or_404(Measurement, id=1)
-    # form = MeasurementModelForm(request.POST or None)
-    geolocator = Nominatim(user_agent='measurements')
-    ip_ = get_ip_address(request)
-    print(ip_)
-    ip = '109.173.220.158'
-    country, city, lat, lon = get_geo(ip)
-    location = geolocator.geocode(city)
-
-    # koordynanty
-    l_lat = lat
-    l_lon = lon
-    pointA = (l_lat, l_lon)
-
-    # inicjowanie mapy
-    m = folium.Map(width='100%', height='100%', location=get_center_coordinates(l_lat, l_lon), zoom_start=8)
-    # znacznik lokalizacji początkowej
-    folium.Marker([l_lat, l_lon], tooltip='twoja lokalizacja',
-                  popup="kod pocztowy: " + city['postal_code'] + " miasto: " + city['city'],
-                  icon=folium.Icon('green')).add_to(m)
-
-    # if form.is_valid():
-    # instance = form.save(commit=False)
-    # destination_ = form.cleaned_data.get('destination')
-    # destination = geolocator.geocode(destination_)
-
-    # destination coordinates
-    # d_lat = destination.latitude
-    # d_lon = destination.longitude
-    # pointB = (d_lat, d_lon)
-
-    # distance calculation
-    # distance = round(geodesic(pointA, pointB).km, 2)
-
-    # initial folium map
-    # m = folium.Map(width='100%', height='100%',
-    #                 location=get_center_coordinates(lat, lon),
-    #                 zoom_start=10)
-    # ,
-    # zoom_start=get_zoom(distance))
-    # location marker
+    print(request.method)
 
     is_ajax = request.headers.get('X-Requested-With') == 'XMLHttpRequest'
+    print(is_ajax)
     if is_ajax:
         if request.method == 'POST':
+            print('1')
+            print(lat)
+            print(lon)
             data = json.load(request)
-            a_lat = data.get('lat')
-            a_lon = data.get('lon')
-            print(a_lat)
-            print(a_lon)
+            n_lat = data.get('lat')
+            n_lon = data.get('lon')
+            if n_lat != lat or n_lon != lon:
+                lat = n_lat
+                lon = n_lon
+                print('2')
+                print(lat)
+                print(lon)
+                return redirect('location-2', n_lat, n_lon)
+        else:
+            return HttpResponse(json.dumps([{'lat':lat, 'lon':lon}]))
 
-            m = folium.Map(width='100%', height='100%',
-                           location=get_center_coordinates(a_lat, a_lon),
-                           zoom_start=10)
+    print('lat:', lat)
+    print('lon:', lon)
 
-            folium.Marker([a_lat, a_lon], tooltip='twoja lokalizacja',
-                          popup="TUTAJ JESTEŚ",
-                          icon=folium.Icon('green')).add_to(m)
+    m = folium.Map(width='100%', height='100%',
+            location=get_center_coordinates(lat, lon),
+            zoom_start=10)
+
+    folium.Marker([lat, lon], tooltip='twoja lokalizacja',
+            popup="Twoja lokalizacja",
+            icon=folium.Icon('green')).add_to(m)
+
 
         # destination  marker
     tasks = (x for x in Task.objects.all() if x.user == request.user)
@@ -103,45 +72,33 @@ def location(request):
         'L': 'lightgray',
         'N': 'white'
     }
-    count = 0
+
     for x in tasks:
-
         if x.l_lon and x.l_lat:
-            folium.Marker([x.l_lat, x.l_lon], tooltip='cel podróży',
-                          popup=x.localization,
-                          icon=folium.Icon(color[x.priority], icon="cloud")).add_to(m)
-        if(x.l_lat != None and x.l_lon != None):
-            diff_lat = abs(float(x.l_lat) - float(lat))
-            diff_lon = abs(float(x.l_lon) - float(lon))
-            if(diff_lat < 0.01 or diff_lon < 0.01):
-                count = count + 1
-    if count > 0:
-        print("Zadania w pobliżu: ", count)
+            folium.Marker([x.l_lat, x.l_lon], tooltip=x.title,
+                    popup=x.localization,
+                    icon=folium.Icon(color[x.priority], icon="cloud")).add_to(m)
 
-        # draw the line between location and destination
-        # line = folium.PolyLine(locations=[pointA, pointB], weight=2, color='blue')
-        # m.add_child(line)
-
-        # folium map modification
-        # instance.location = location
-        # instance.distance = distance
-        # instance.save()
 
     m = m._repr_html_()
     # distance = None
 
     context = {
-        # 'distance': distance,
-        # 'destination': destination,
-        # 'form': form,
-        user: user,
-        'vapid_key': vapid_key,
+        'lat': lat,
+        'lon': lon,
         'map': m,
     }
 
-    print(vapid_key, user)
+    t = loader.get_template('geolocation/location.html')
+    return HttpResponse(t.render(context, request))
 
-    return render(request, 'geolocation/location.html', context)
+
+def start(request):
+    ip_ = get_ip_address(request)
+    print(ip_)
+    ip = '109.173.220.158'
+    country, city, lat, lon = get_geo(ip)
+    return redirect('location-2', lat, lon)
 
 
 #def send_push(request):
