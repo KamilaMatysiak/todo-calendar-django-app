e3eee0c63b4db31cd6b60c588f14f6d0d3bf4c61
Sergio <serhro@st.amu.edu.pl>
2021-11-20 10:07:22 +0200

    add superuser init command

---
+++ users/management/commands/initadmin.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+from django.conf import settings
+import os
+
+UserModel = get_user_model()
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        try:
+            UserModel.objects.create_superuser(username='admin',
+                                               email='admin@v-todo.com',
+                                               password='admin',  # change on genenv("ADMIN_PASSWORD")
+                                               )
+        except Exception as e:
+            print("Got following exception: ", e)
